accf5014c394d4984c379a9e08400371e69080f3
Jonas Scholl <43819845+JonasScholl@users.noreply.github.com>
2025-10-25 19:36:51 +0200

    setup iconic

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hitster',
+  webDir: 'dist'
+};
+
+export default config;
